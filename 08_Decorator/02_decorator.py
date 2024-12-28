bc19b56dc2e91cd2ec5c6899e19d6e868f8450f5
SohamPethkar7038 <sohampethkar04@gmail.com>
2024-12-28 18:20:08 +0530

    Decorator files

---
+++ 08_Decorator/02_decorator.py
@@ -0,0 +1,14 @@
+# Problem 2: Debugging Function Calls
+
+# Problem: Create a decorator to print the function name and the values of its arguments every time the function is called.
+
+
+
+
+
+def greet(name,greeting="hello"):
+    print(f"{greeting},{name}")
+    
+
+greet("Chai",greeting="hanji");
+    
